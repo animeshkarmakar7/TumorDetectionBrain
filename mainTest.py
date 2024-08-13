5cada069f8aa173ecf60ba98f183394126129872
Animesh Karmakar <animeshkarmakar992@gmail.com>
2024-08-13 23:38:57 +0530

    first commit

---
+++ mainTest.py
@@ -0,0 +1,29 @@
+import cv2
+from keras.models import load_model
+from PIL import Image
+import numpy as np
+
+model=load_model('BrainTumor10EpochsCategorical.h5')
+
+image=cv2.imread('D:\\tumour detection\\pred\\pred5.jpg')
+
+img=Image.fromarray(image)
+
+img=img.resize((64 , 64))
+
+img=np.array(img)
+
+img = img / 255.0
+img = np.expand_dims(img, axis=0)  # Expanding to match the input shape of the model (1, 64, 64, 3)
+
+# Predict the class
+result = model.predict(img)
+
+# For binary classification
+if result[0][0] > 0.5:
+    print("Yes, tumor detected")
+else:
+    print("No tumor detected")
+
+
+
